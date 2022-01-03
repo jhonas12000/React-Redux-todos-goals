5e6ac1e79b8e1cd95b65f60552abd5b527cd022c
jhonas12000 <ytwmichael@gmail.com>
2022-01-03 07:47:25 -0800

    The store with state tree has been created.

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ function createStore () {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
   return {
-    getState
+    getState,
+    subscribe
   }
 }
+
+const store = createStore() 
+store.sugscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+const unsubscribe = store.subscribe(() => {
+  console.log('The store changed.')
+})
