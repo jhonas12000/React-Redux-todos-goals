195ce37217c230ca644dcbfa5aa1ea55ea6d2236
jhonas12000 <ytwmichael@gmail.com>
2022-01-05 07:53:11 -0800

    todos and goals added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore () {
+// Library Code
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -17,16 +18,38 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch,
   }
 }
 
-const store = createStore() 
-store.sugscribe(() => {
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+const store = createStore(todos)
+
+store.subscribe(() => {
   console.log('The new state is: ', store.getState())
 })
-const unsubscribe = store.subscribe(() => {
-  console.log('The store changed.')
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
 })
